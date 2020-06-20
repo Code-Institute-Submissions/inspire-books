58ead9d018ab5e04016cb8067249e8871d375476
Jude <storeyjude@hotmail.com>
2020-06-20 14:54:38 +0000

    initial commit for heroku

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Inspire Book Repo Coming Soon'
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
