ea1d0a5648c20ce94c9d33ec07be28dab6c745e1
Jude <storeyjude@hotmail.com>
2020-06-20 15:18:34 +0000

    updated app file and connected to mongo & heroku

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'book_repo'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-sknof.mongodb.net/book_repo?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Inspire Book Repo Coming Soon'
+@app.route('/get_base')
+def get_base():
+    return render_template("base.html", base=mongo.db.book_title.find()) #change later to appropiate mongo file
 
 
 if __name__ == '__main__':
