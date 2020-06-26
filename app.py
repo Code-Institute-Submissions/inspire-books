a7d1a2a3195398acb9df7301a1520820ca17ac10
Jude <storeyjude@hotmail.com>
2020-06-26 21:59:57 +0000

    created new data from mongo trying to correct layout

--- app.py
+++ app.py
@@ -12,7 +12,12 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_books')
 def get_books():
-    return render_template("books.html", books=mongo.db.book_title.find()) #change later to appropiate mongo file
+    return render_template("books.html", 
+                            books=mongo.db.book_title.find(),
+                            categories=mongo.db.categories.find(),
+                            inspire_person=mongo.db.inspire_person.find())
+
+
 
 
 if __name__ == '__main__':
