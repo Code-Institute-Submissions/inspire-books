dc2a1a838bc44152c9312672a08285f67c5a3ccd
Jude <storeyjude@hotmail.com>
2020-07-01 20:26:17 +0000

    tidy up of structure and added data to fill out

--- app.py
+++ app.py
@@ -9,28 +9,38 @@ app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-sknof.mong
 
 mongo = PyMongo(app)
 
+
 @app.route('/')
 @app.route('/get_home')
 def get_home():
-    return render_template('home.html')
-
+    return render_template('home.html',
+                            home=mongo.db.book_title.find(),
+                            inspire_person=mongo.db.inspire_person.find())
 
 @app.route('/get_books')
 def get_books():
-    return render_template("books.html", 
-                           books=mongo.db.book_title.find(),
-                           categories=mongo.db.categories.find(),
+    return render_template("books.html",
+                            books=mongo.db.book_title.find(),
+                            categories=mongo.db.categories.find(),
                             inspire_person=mongo.db.inspire_person.find())
 
 
 @app.route('/get_people')
 def get_people():
-    return render_template('people.html')
+    return render_template('people.html',
+                            books=mongo.db.book_title.find(),
+                            inspire_person=mongo.db.inspire_person.find())
+
+@app.route('/get_featured_books')
+def get_featured_books():
+    return render_template('featured_books.html', 
+                            books=mongo.db.book_title.find())
+
+
 
 @app.route('/get_submission')
 def get_submission():
     return render_template('submission.html')
-
 
 
 if __name__ == '__main__':
